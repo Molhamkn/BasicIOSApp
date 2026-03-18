f7b1b4860ef78fc00ebcd263ec143eabf7e47273
Molhamkn <Molhamkn@users.noreply.github.com>
2026-03-18 16:25:53 -0400

    Simplify app

--- Sources/BasicIOSAppApp.swift
+++ Sources/BasicIOSAppApp.swift
@@ -4,7 +4,8 @@ import SwiftUI
 struct BasicIOSAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("Hello iOS!")
+                .padding()
         }
     }
 }
